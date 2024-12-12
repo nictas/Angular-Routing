021a480dff10740bdee9577edf40d5c00d2b6ad2
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-12-12 17:09:03 +0200

    Chapter 4.5

--- APM-Start v19/src/app/user/login.component.ts
+++ APM-Start v19/src/app/user/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class LoginComponent {
   errorMessage = '';
   pageTitle = 'Log In';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   login(loginForm: NgForm): void {
     if (loginForm && loginForm.valid) {
@@ -19,7 +20,7 @@ export class LoginComponent {
       const password = loginForm.form.value.password;
       this.authService.login(userName, password);
 
-      // Navigate to the Product List page after log in.
+      this.router.navigate(['/products']);
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
