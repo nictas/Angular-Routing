66cf5b5d48f3ddd3700a3c7491a1d2de369d7261
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-12-12 16:52:09 +0200

    Chapter 4.2

--- APM-Start v19/src/app/products/product.module.ts
+++ APM-Start v19/src/app/products/product.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 
-import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductListComponent } from './product-list.component';
 
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+    ])
   ],
   declarations: [
     ProductListComponent,
