ecb0dc329ac2f3f7fd77962ca54809b03947b114
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-12-16 19:20:55 +0200

    Chapter 5.10

--- APM-Start v19/src/app/products/product-list.component.ts
+++ APM-Start v19/src/app/products/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -27,9 +28,16 @@ export class ProductListComponent implements OnInit {
   filteredProducts: Product[] = [];
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe(queryParams => {
+      this.listFilter = queryParams.get('filterBy') || '';
+      this.showImage = queryParams.get('showImage') === 'true';
+    })
     this.productService.getProducts().subscribe({
       next: products => {
         this.products = products;
