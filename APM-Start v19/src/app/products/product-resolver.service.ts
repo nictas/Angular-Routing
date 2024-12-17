8704cb06b97269537f3174f94ba504608d3c9410
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-12-17 17:13:12 +0200

    Chapter 6.5

--- APM-Start v19/src/app/products/product-resolver.service.ts
+++ APM-Start v19/src/app/products/product-resolver.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Product } from './product';
+import { catchError, map, Observable, of } from 'rxjs';
+import { ProductResolved } from './product';
 import { ProductService } from './product.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ProductResolverService implements Resolve<Product> {
+export class ProductResolverService implements Resolve<ProductResolved> {
 
     constructor(private productService: ProductService) { }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
-        const id = Number(route.paramMap.get('id'));
-        return this.productService.getProduct(id);
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductResolved> {
+        const id = route.paramMap.get('id');
+        if (isNaN(Number(id))) {
+            const message = `Product ID is not a number: ${id}`;
+            return of({ product: null, error: message });
+        }
+        return this.productService.getProduct(Number(id)).pipe(
+            map(product => ({ product: product })),
+            catchError(error => {
+                const message = `Error while retrieving product: ${error}`;
+                return of({ product: null, error: message });
+            })
+        );
     }
+
 }
