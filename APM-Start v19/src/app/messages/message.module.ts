99ff6ba46a956949334a60d98189e7e12e23a78f
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2025-01-29 09:01:55 +0200

    Chapter 10.4

--- APM-Start v19/src/app/messages/message.module.ts
+++ APM-Start v19/src/app/messages/message.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { RouterModule } from '@angular/router';
 import { MessageComponent } from './message.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'messages',
+        component: MessageComponent,
+        outlet: 'popup'
+      }
+    ])
   ],
   declarations: [
     MessageComponent
