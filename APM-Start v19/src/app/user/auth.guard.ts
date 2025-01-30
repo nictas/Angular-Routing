2746be0ca9b14c7630225f38b15cc586f988d7ec
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2025-01-30 13:05:57 +0200

    Chapter 11.4

--- APM-Start v19/src/app/user/auth.guard.ts
+++ APM-Start v19/src/app/user/auth.guard.ts
@@ -16,6 +16,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isLoggedIn) {
       return true;
     }
+    this.authService.redirectUrl = state.url;
     this.router.navigate(['/login']);
     return false;
   }
--- APM-Start v19/src/app/user/auth.service.ts
+++ APM-Start v19/src/app/user/auth.service.ts
@@ -8,6 +8,7 @@ import { MessageService } from '../messages/message.service';
 })
 export class AuthService {
   currentUser?: User | undefined;
+  redirectUrl?: string;
 
   get isLoggedIn(): boolean {
     return !!this.currentUser;
